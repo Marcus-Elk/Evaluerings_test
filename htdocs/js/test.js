3bf989e337cda7b872e2ffee64e19ed7ee12a542
Tegon-McClouds <Tegon.mccloud@gmail.com>
2020-11-05 12:49:13 +0100

    add animations to test.php

--- htdocs/js/test.js
+++ htdocs/js/test.js
@@ -2,20 +2,19 @@
 $(document).ready(function() {
 	
 	$(".question-toggle").each(function(index) {
-		$(this).click(function() {
-			
-			let content = $(this).next();
 
-			if(content.css("max-height") == "0px") {
-				content.css("max-height", "none");
+		$(this).click(function() {
+			$(this).toggleClass("expanded");
+
+			let content = $(this).next()[0];
+
+			if (content.style.maxHeight){
+				content.style.maxHeight = null;
 			} else {
-				content.css("max-height", "0px");
+				content.style.maxHeight = content.scrollHeight + "px";
 			}
 		});
 	});
-
-
-
 
 });
 
