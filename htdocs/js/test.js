46a7e4e2a4ee263c13dfe02932a7fc9823ebe153
Tegon-McCloud <38768974+Tegon-McCloud@users.noreply.github.com>
2020-11-05 15:46:53 +0100

    more styling

--- htdocs/js/test.js
+++ htdocs/js/test.js
@@ -15,7 +15,26 @@ $(document).ready(function() {
 	});
 
 
+	$(".submit-button").click(function() {
+		let test = {
+			questions: []
+		}
 
+		$(".question").each(function(index) {
+			let q_id = this.id.substring(1);
+			let a_id = $("input[name="+q_id+"]:checked").val();
+
+			let question = {
+				id: q_id,
+				answer: a_id
+			}
+
+			test.questions.push(question);
+		});
+
+		// TODO: send test to server
+
+	});
 
 });
 
