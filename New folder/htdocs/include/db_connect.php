8565eedd983d39e04c5c30ed13becb4cfe6f4a27
Muneeb2002 <mmuneebfar@gmail.com>
2020-11-13 10:50:26 +0100

    slet knap virker

--- New folder/htdocs/include/db_connect.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-	$db = mysqli_connect("localhost:3306", "root", "", "grfl_mat");
-	if(mysqli_connect_errno($db) > 0) {
-		die("Unable to connect to database: " . mysqli_connect_error($db));
-	}
-?>
